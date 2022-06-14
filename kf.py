e0e0ab4e839b7c5bc8ee91955816c000819d58a8
Abhigyan Bhusal <bhusalabhigyan@gmail.com>
2022-06-14 22:25:39 +0545

    Remove old code

--- kf.py
+++ kf.py
@@ -75,57 +75,6 @@ class TwoDRandomWalkSimulation(object):
         pygame.quit()
 
 
-def main():
-    x = 300
-    y = 300
-    theta = 0.3
-    v = 1
-    to_loop = True
-    random_scale = 0.6
-    DEQUE_MAXLEN = 100
-    pygame.init()
-
-    game_display = pygame.display.set_mode((800, 600))
-    pygame.display.set_caption("Going in circles")
-    game_display.fill(GRAY)
-
-    clock = pygame.time.Clock()
-
-    xs = deque([], maxlen=DEQUE_MAXLEN)
-    xs.append(x)
-    ys = deque([], maxlen=DEQUE_MAXLEN)
-    ys.append(y)
-
-    while to_loop:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                to_loop = False
-            print(event)
-
-        game_display.fill(GRAY)
-        theta = theta + (random.random() - 0.5) * random_scale
-        x = x + v * math.cos(theta)
-        y = y + v * math.sin(theta)
-
-        xs.append(x)
-        ys.append(y)
-
-        _color = list(RED)
-        factor = 1 / DEQUE_MAXLEN
-        for _x, _y in zip(reversed(xs), reversed(ys)):
-            _color[0] = int(RED[0] * (1 - factor) + GRAY[0] * factor)
-            _color[1] = int(RED[1] * (1 - factor) + GRAY[1] * factor)
-            _color[2] = int(RED[2] * (1 - factor) + GRAY[2] * factor)
-            factor += 1 / DEQUE_MAXLEN
-            pygame.draw.circle(game_display, tuple(_color), (_x, _y), 2)
-
-        pygame.display.update()
-        clock.tick(dt * 1000)
-
-    pygame.quit()
-    quit()
-
-
 if __name__ == "__main__":
     go = TwoDRandomWalkSimulation()
     go.start_game_thread()
